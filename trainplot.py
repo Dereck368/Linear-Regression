d7820e7aea666bba20b261a2be9571c9e3031b3a
Dereck368 <derecktaverne@gmail.com>
2024-10-27 16:24:10 -0400

    finished improving model by adding non-linear features, added a few comments to check what I need to do to refactor the function

--- trainplot.py
+++ trainplot.py
@@ -3,6 +3,9 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import statsmodels.api as sm
 
+from sklearn.feature_selection import SequentialFeatureSelector
+import numpy as np
+
 def get_DataFrame():
     columns = ["Temperature", "Icecream"]
     df = pd.read_fwf('train.txt', header=None, names=columns)
@@ -42,9 +45,68 @@ def stats_summary(X, y):
 
     return results.summary()
 
+# fix discription and variable names of function
+def model(df):
+    x = df["Temperature"].to_numpy()
+    y = df["Icecream"].to_numpy()
+
+    epsilon = 1e-8
+
+    # different features
+    X = pd.DataFrame({
+    'x': x,
+    'cos_x': np.cos(x),
+    'log_x': np.log(x + epsilon), #avoid log(0)
+    'cos_4x': np.cos(4 * x),
+    'sin_3x': np.sin(3 * x),
+    'sin_5x': np.sin(5 * x),
+    'sin_2x_cos_2x': np.sin(2 * x) * np.cos(2 * x)
+    })
+
+    #run selction on num of features get list of each M0 M1 etc...
+    print(X)
+    p = X.shape[1] - 1
+    #help with indexing error without '- 1'
+    # ok so we do 7 choose 6 features to add that's why it is -1
+    # only problem here is if if we want to choose 6 features wouldn't it get rid of 
+    # x
+        # if it does get rid of x does that mean that x wasn't an optimal feature
+        # for a solution.
+
+    best_features = []
+    best_adjusted_r2 = -np.inf
+
+    for k in range(1, p+1):
+        model = LinearRegression()
+
+        selector = SequentialFeatureSelector(model, n_features_to_select=k, direction='forward', scoring='r2')
+        selector.fit(X, y)
+        
+        # gets the selected features
+        features_selected = selector.get_support(indices=True)
+        X_train = X.iloc[:, features_selected]
+        
+        model.fit(X_train, y)
+        r2 = model.score(X_train, y)
+
+        n = X.shape[0]
+        d = X_train.shape[1]
+
+        adjusted_r2 = 1 - ((1 - r2) * (n - 1) / (n - d - 1))
+
+        if adjusted_r2 > best_adjusted_r2:
+            best_adjusted_r2 = adjusted_r2
+            best_features = features_selected
+
+    print(X.columns[best_features])
+    print(adjusted_r2)
+
+    # create return method
+    # graph adjusted r2 of each model
+        #to do this create list for models then graph each one
+
 def main():
     df = get_DataFrame()
-    tdf = get_test_df()
 
     # plots train data
     # scatter_plot_DataFrame(df)
@@ -57,30 +119,15 @@ def main():
 
     # print beta's
     beta0_hat, beta1_hat = linReg(x, y)
-    print()
-    print(f'β̂0 (Slope): {beta0_hat}) \nβ̂1 (Intercept): {beta1_hat} )')
-    print()
+    # print()
+    # print(f'β̂0 (Slope): {beta0_hat}) \nβ̂1 (Intercept): {beta1_hat} )')
+    # print()
+
+    model(df)
     
     # get StatsModel
     res = stats_summary(x, y)
-    print(res)
+    # print(res)
 
-    # Can you conclude that there is a linear trend in the data? This is equivalent to ask whether you
-    # can conclude that β1 is equal to zero or not. Justify your answer using statistical inference.
-
-        # From this StatsModel we can conclude that their is a linear Trend in the data.
-        # The reason for this is because our H₀: β̂1 = 0 (No linear trend)
-        # and our H₁: β̂1 != 0 (there is a linear trend)
-        # we have a p value of 0.00
-        # the p value is less then the signifance level so we can reject the null hypothesis
-        # because we rejected the null hyposthesis we can conclude that β̂1 != 0
-        # which means their is a linear trend in the data
-
-    # What about β0, can you conclude whether this parameter is zero or not?
-
-        # H₀: β̂0 = 0
-        # and our H₁: β̂0 != 0
-        # We can conlude that it isn't zero because hte p value is less then the signifance level,
-        # rejecting teh null hypothesis β̂0 != 0
 
 main()
